d1c9bcf8e25f1577a0302969267f30d0a45ab7da
WilliamNyamu <williamnyamu08@gmail.com>
2024-09-05 18:12:53 +0300

    Second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Header from './header';
 import Verse from './verse';
 import Ministry from './ministry';
 import Footer from './footer.js';
+import cards from './ministry.js';
 function App() {
   return (
     <div>
---
+++ my-app/src/data.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        image : "people_praying.jpg",
+        details : "During the days of Jesus on earth, He offered up prayers and petitions with fervent cries and tears to the one who could save him from death, and he was heard because of his reverent submission",
+        location : "The famous: SR13",
+        time_details : "0500hrs - 0620hrs"
+    },
+    {
+        image : "guitar.webp",
+        details : "Let everything that has breath praise the Lord!!",
+        location : "The famous: SR13",
+        time_details : "0500hrs - 0620hrs"
+    },
+    {
+        image : "media_ministry.jpg",
+        details : "The Media Ministry in a church enhances worship by managing audio, visual, and digital media. It operates sound systems, projects lyrics, records and streams services, and oversees the church's online presence. ",
+        location : "The famous: SR13",
+        time_details : "0500hrs - 0620hrs"
+    },
+    {
+        image : "PW.jpg",
+        details : "In the praise and worship team, you lead and support singing, helping to create an engaging and spiritually uplifting worship experience.",
+        location : "The famous: SR13",
+        time_details : "0500hrs - 0620hrs"
+    },
+    {
+        image : "intercessors.png",
+        details : "During the days of Jesus on earth, He offered up prayers and petitions with fervent cries and tears to the one who could save him from death, and he was heard because of his reverent submission",
+        location : "The famous: SR13",
+        time_details : "0500hrs - 0620hrs"
+    }
+]
+export default data;
